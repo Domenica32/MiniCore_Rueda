21dc021cdb077bdb9d46031c3cc0d63264a26d41
Domenica16 <domenica.rueda@udla.edu.ec>
2022-10-24 15:03:47 -0500

    url

--- ProyectoBonos_Rueda/frontend/src/main.js
+++ ProyectoBonos_Rueda/frontend/src/main.js
@@ -10,4 +10,4 @@ import VueAxios from 'vue-axios';
 
 createApp(App).use(VueAxios, axios).mount('#app')
 
-axios.defaults.baseURL = 'https://aqueous-headland-58640.herokuapp.com/'
+axios.defaults.baseURL = 'https://aqueous-headland-58640.herokuapp.com/api/'
